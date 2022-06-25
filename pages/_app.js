6001fe9513cf412b85fe2ce61c640002df5a72b1
Pushkaraj Bhor <80631748+Epsi2K@users.noreply.github.com>
2022-06-25 03:16:35 +0000

    Starter Push

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
+import {ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
 import '../styles/globals.css'
+import {ThemeProvider} from 'next-themes'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider
+      desiredChainId = {ChainId.Rinkeby}
+        chainRpc={{
+          [ChainId.Rinkeby]:'https://rinkeby.infura.io/v3/27fa21f024d44996b9b7e2d97d59d402'
+        }}
+      >
+      <ThemeProvider>
+         <Component {...pageProps} /> 
+      </ThemeProvider>
+     
+    </ThirdwebProvider>
+    
+  )
 }
 
 export default MyApp
